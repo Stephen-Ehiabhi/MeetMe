a04282569d3fdf085052fb70a4aa961c63d98eb6
stephenehiabhi <steveyyh123@gmail.com>
2022-03-16 18:38:33 +0100

    finalised the main backend logic for creating a chat page

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app)
-const {Server} = require("socket.io")
+const { Server } = require("socket.io")
 const io = new Server(server);
 
 //module to hide sensitive data
 const { PORT } = require("./config/config.js");
 
-io.on('connection', (socket)=>{
-    console.log("Socket established!");
-    socket.on("message", (msg)=>{
-        socket.broadcast.emit("chat-message", {message: msg})
+io.on('connection', (socket) => {
+    socket.broadcast.emit("user-connected", "Online" )
+    socket.on("message", (msg) => {
+        socket.broadcast.emit("chat-message", { message: msg })
     })
-    socket.on("disconnect",()=>{
-        console.log("Socket terminated!");
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("user-disconnected", "Offline" )
     })
 })
 
-app.get('/', (req,res)=>{
-   res.sendFile(__dirname + "/index.html")
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
 })
 
 //intiating the listen method to enable server listen for requests
