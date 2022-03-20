f06479303b1f923fe410031d0b27f150eafe0445
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-03-20 16:09:23 +0100

    connected the server to a database

--- server/server.js
+++ server/server.js
@@ -1,29 +1,33 @@
+//module to hide sensitive data
+const { PORT, URI } = require("./config/config.js");
+
 const express = require("express");
 const app = express();
 const http = require("http");
-const server = http.createServer(app)
-const { Server } = require("socket.io")
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 const io = new Server(server);
+const { connect } = require("mongoose");
 
-//module to hide sensitive data
-const { PORT } = require("./config/config.js");
+//const userRo
 
-io.on('connection', (socket) => {
-    socket.broadcast.emit("user-connected", "Online" )
-    socket.on("message", (msg) => {
-        socket.broadcast.emit("chat-message", { message: msg })
-    })
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("user-disconnected", "Offline" )
-    })
-})
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
+//conecting database and server
+connect(URI, (err) => {
+  if (err) console.log(`Error listening on Port ${PORT}`);
+  //intiating the listen method to enable server listen for requests
+  server.listen(PORT, () => {
+    console.log(`Server and DB connected on Port ${PORT}`);
+  });
+});
 
-//intiating the listen method to enable server listen for requests
-server.listen(PORT, (err) => {
-    if (err) console.log(`Error listening on Port ${PORT}`);
-    console.log(`Server listening on Port ${PORT}`);
-})
+//initializing the connection event on the socket server
+io.on("connection", (socket) => {
+  socket.broadcast.emit("user-connected", "Online");
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("chat-message", { message: msg });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", "Offline");
+  });
+});
