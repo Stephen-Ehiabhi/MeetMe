831de56bce64f3f18fa49b22c93696bee6698709
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-03-20 13:02:52 +0100

    added readme file

---
+++ server/controllers/conversation-controller.js
@@ -0,0 +1,13 @@
+const conversation = require("../models/Conversation");
+const {createOne,readAll,readOne,updateOne,deleteOne} = require("../services/crud");
+const response = require("../services/response");
+
+module.exports = {
+  createConversation: (req,res) => {
+    try {
+             
+    } catch (error) {
+             response(req,)
+    }
+  }
+}
