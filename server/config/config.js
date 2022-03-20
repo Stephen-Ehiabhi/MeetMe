293e23c573c89a7ff1aabe930198220437084176
steevgr3y <steveyyh123@gmail.com>
2022-03-20 16:11:46 +0100

    Delete config.js

--- server/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: 3000 || process.env.PORT,
-    URI: "mongodb+srv://stephen:Monammie01@cluster0.0qj1q.gcp.mongodb.net/meetme?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
-}
