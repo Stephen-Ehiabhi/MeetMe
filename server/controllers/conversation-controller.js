4e0814b5742b82222e9dcf64d92ca275ee7eb363
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-03-20 13:11:55 +0100

    added the crud activities for the conversation controller

--- server/controllers/conversation-controller.js
+++ server/controllers/conversation-controller.js
@@ -1,13 +1,37 @@
 const conversation = require("../models/Conversation");
-const {createOne,readAll,readOne,updateOne,deleteOne} = require("../services/crud");
+const {
+  createOne,
+  readAll,
+  readOne,
+  updateOne,
+  deleteOne,
+} = require("../services/crud");
 const response = require("../services/response");
 
 module.exports = {
-  createConversation: (req,res) => {
+  //create
+  createConversation: (req, res) => {
     try {
-             
+      createOne(req, conversation);
+      response(res, 200, "new post created");
     } catch (error) {
-             response(req,)
+      response(res, 400, error);
     }
-  }
-}
+  },
+  //read all
+  readAllConversations: (req, res) => {
+    readAll(res, conversation);
+  },
+  //read one
+  readConversation: (req, res) => {
+    readOne(req, res, conversation);
+  },
+  //update one
+  updateConversation: (req, res) => {
+    // updateOne(req,res, conversation,key,value);
+  },
+  //delete one
+  deleteConversation: (req, res) => {
+    deleteOne(req, res, conversation);
+  },
+};
