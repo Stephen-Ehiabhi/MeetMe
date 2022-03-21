fa85667aa3c32cbdd21dcf4ca36384e359a97a70
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-03-21 09:44:49 +0100

    added the message model to store messages

---
+++ server/models/Message.js
@@ -0,0 +1,18 @@
+const {Schema, model} = require("mongoose");
+
+const MessageSchema = new Schema(
+  {
+    conversationId: {
+      type: String,
+    },
+    sender: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Message", MessageSchema);
