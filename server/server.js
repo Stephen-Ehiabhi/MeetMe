63d971cd302c6de0bcc0b93fa084c19fbdc97018
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-03-21 10:01:07 +0100

    moved the socket server to a new directory, and added functionalities

--- server/server.js
+++ server/server.js
@@ -1,26 +1,21 @@
 const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+app = express();
 const { connect } = require("mongoose");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const cors = require("cors");
+userRoutes = require("./routes/user");
+postRoutes = require("./routes/post");
+convoRoutes = require("./routes/conversation");
+helmet = require("helmet");
+morgan = require("morgan");
+cors = require("cors");
 
 //module to hide sensitive data
 require("dotenv").config();
-const PORT = process.env.PORT;
+PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 app.use(cors());
-
-const userRoutes = require("./routes/user");
-const postRoutes = require("./routes/post");
-const convoRoutes = require("./routes/conversation");
 
 app.use("/api/user", userRoutes);
 app.use("/api/", postRoutes);
@@ -33,19 +28,8 @@ connect(
   (err) => {
     if (err) console.log(`Error listening on Port ${PORT}`);
     //intiating the listen method to enable server listen for requests
-    server.listen(PORT, () => {
+    app.listen(PORT, () => {
       console.log(`Server and DB connected on Port ${PORT}`);
     });
   }
 );
-
-//initializing the connection event on the socket server
-io.on("connection", (socket) => {
-  socket.broadcast.emit("user-connected", "Online");
-  socket.on("message", (msg) => {
-    socket.broadcast.emit("chat-message", { message: msg });
-  });
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", "Offline");
-  });
-});
